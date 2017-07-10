dfa52dfe2f464a67780f9bd1692f6a15ddb5cb5d
Shane Smith <shane@communityds.com.au>
2017-07-10 12:29:19 +0930

    ShowID - Removed comment

--- ShowID.js
+++ ShowID.js
@@ -1,8 +1,3 @@
-/**
- * Purpose: Make the Task ID easily accessible in a copy-paste compatible way, without opening the task directly.
- * How to use: Click the bookmark while the "My Work" tab is open.
- */
-
 javascript:
 var tasks=document.getElementsByClassName('task_row large_row');
 for(var i=0;i < tasks.length; i++){
